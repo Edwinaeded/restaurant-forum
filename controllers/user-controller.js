1d066358d3ffcd30f9b8f073c7d19d485376dfbd
Edwinaeded <edwinaeded@gmail.com>
2024-12-05 21:01:02 +0800

    Feat : flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,18 +6,24 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-      .then((hash) => {
-        return User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: hash
-        })
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    User.findOne({ where: { email: req.body.email } })
+      .then((user) => {
+        if (user) throw new Error('Email already exists!')
+
+        return bcrypt.hash(req.body.password, 10)
       })
+      .then(hash => User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      }))
       .then(() => {
+        req.flash('success', '成功註冊帳號！')
         return res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,16 @@ const router = express.Router()
 const userController = require('../controllers/user-controller')
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const { generalErrorHandler } = require('../middlewares/error-handler')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
+
 router.use('/', (req, res) => {
   res.redirect('/restaurants')
 })
+router.use('/', generalErrorHandler)
 
 module.exports = router
