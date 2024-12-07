65dbe9efe7ab52ceabd42fdfe77667258fc3444a
Edwinaeded <edwinaeded@gmail.com>
2024-12-08 01:13:53 +0800

    Feat : create restaurant

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const passport = require('../config/passport')
 
 const userController = require('../controllers/user-controller')
 const restController = require('../controllers/restaurant-controller')
-const { authenticated } = require('../middlewares/auth')
+const { authenticated, adminAuthenticated } = require('../middlewares/auth')
 const admin = require('./modules/admin')
 const { generalErrorHandler } = require('../middlewares/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', adminAuthenticated, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
-const { adminAuthenticated } = require('../../middlewares/auth')
 
-router.get('/restaurants', adminAuthenticated, adminController.getRestaurants)
+router.get('/restaurants', adminController.getRestaurants)
+router.get('/restaurants/create', adminController.createRestaurant)
+router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => {
   res.redirect('/admin/restaurants')
 })
