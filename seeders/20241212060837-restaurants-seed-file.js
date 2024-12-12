86e81b3ecc13a61f736bc8a5acc605f70d67a986
Edwinaeded <edwinaeded@gmail.com>
2024-12-12 14:19:58 +0800

    Feat : update seed files

--- seeders/20241212060837-restaurants-seed-file.js
+++ seeders/20241212060837-restaurants-seed-file.js
@@ -3,16 +3,18 @@ const { faker } = require('@faker-js/faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const categories = await queryInterface.sequelize.query('SELECT id FROM Categories;', { type: queryInterface.sequelize.QueryTypes.SELECT })
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.company.name(),
         tel: faker.phone.number(),
-        address: faker.address.streetAddress(),
+        address: faker.location.streetAddress(),
         opening_hours: '08:00',
         image: `https://loremflickr.com/320/240/food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
