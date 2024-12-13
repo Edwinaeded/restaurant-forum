999c1d30a9e5335cef36a94012dd592e02599ddb
Edwinaeded <edwinaeded@gmail.com>
2024-12-13 17:53:31 +0800

    Feat : add category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -7,6 +7,18 @@ const categoryController = {
         return res.render('admin/categories', { categories })
       })
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+
+    if (!name) throw new Error('Category name is required!')
+
+    Category.create({ name })
+      .then(() => {
+        req.flash('success_messages', 'Category has been created successfully!')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
   }
 }
 
