1d3b9606ebdbe36e46a8be855059b9f65c2a59e3
Edwinaeded <edwinaeded@gmail.com>
2024-12-19 02:23:32 +0800

    Feat : get user's favorited restaurants & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -24,9 +24,11 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, 50)
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id)
         }))
         console.log(getPagination(limit, page, restaurants.count))
         return res.render('restaurants', {
@@ -42,14 +44,18 @@ const restaurantController = {
     Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-        { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' }
       ]
     })
       .then(restaurant => {
         console.log(restaurant.Comments)
         if (!restaurant) throw new Error('Restaurant do not exist!')
         return restaurant.increment('viewCounts')
-          .then(() => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      })
+      .then(restaurant => {
+        const isFavorited = restaurant.FavoritedUsers.some(user => user.id === req.user.id)
+        res.render('restaurant', { restaurant: restaurant.toJSON(), isFavorited })
       })
       .catch(err => next(err))
   },
